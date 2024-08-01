c344b1a6f4ff49a901f98b444e4411d6a5a85d9c
mzfirozuddin <mz.firozuddin@gmail.com>
2024-08-02 01:01:08 +0530

    add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.8] antialiased bg-grid-white/[0.02]">
-      <h1>Music Academy</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
     </main>
   );
 }
